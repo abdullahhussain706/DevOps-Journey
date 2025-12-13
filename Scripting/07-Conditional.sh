19e83c0682fc7e8e923269481f008c8bc042c917
abdullahhussain706 <abdullah706706@gmail.com>
2025-12-13 12:00:00 +0500

    Conditional Structure in Scripting

---
+++ Scripting/07-Conditional.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+Log_Dir="/home/muhammad-abdullah/Bash"
+
+ERROR_PATTERN=("ERROR" "FATAL" "CRITICAL")
+REPORT_FILE="/home/muhammad-abdullah/Report.txt"
+
+echo "analyzing log files" > "$REPORT_FILE"
+echo "_______________" >> "$REPORT_FILE"
+
+echo -e "\nfound log files " >> "$REPORT_FILE"
+LOG_FILES=$(find $Log_Dir -name "*.log")
+echo "$LOG_FILES" >> "$REPORT_FILE"
+
+for LOG_FILE in $LOG_FILES; do
+
+    echo -e "\n" >> "$REPORT_FILE"
+    echo "============================" >> "$REPORT_FILE"
+    echo "========$LOG_FILE===========" >> "$REPORT_FILE"
+    echo "============================" >> "$REPORT_FILE"
+
+    for PATTERN in "${ERROR_PATTERN[@]}"; do
+        echo -e  "\nsearching $PATTERN in $LOG_FILE file" >> "$REPORT_FILE"
+        grep "$PATTERN" "$LOG_FILE" >> "$REPORT_FILE"
+
+        echo -e  "\nNumber of $PATTERN found in $LOG_FILE file" >> "$REPORT_FILE"
+        ERROR_COUNT=$(grep -o "$PATTERN" "$LOG_FILE" | wc -l)
+        echo "$ERROR_COUNT" >> "$REPORT_FILE"
+
+        if [ "$ERROR_COUNT" -gt 10 ]; then
+            echo -e "\n⚠️ Attention Required: Too many $PATTERN issues in log file $LOG_FILE"
+        fi
+    done
+done
+
+echo -e "\nLog Analysis Completed and Saved in: $REPORT_FILE"
