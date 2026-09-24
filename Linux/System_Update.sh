bdb7c12acfa6091c2d95329b147c5fb608a45bc0
abdullahhussain706 <abdullah706706@gmail.com>
2026-09-24 14:12:34 +0500

    add url shortener CI workflow and updates

--- Linux/System_Update.sh
+++ Linux/System_Update.sh
@@ -6,35 +6,40 @@ LOG_FILE="./update.log"
 
 echo "Update started at $(date)" >> "$LOG_FILE"
 
-
 echo "----------------------------------------" >> "$LOG_FILE"
 echo "Maintenance started at $(date)" >> "$LOG_FILE"
 
 # Update package list
 echo "Updating package lists..." >> "$LOG_FILE"
-sudo apt update >> "$LOG_FILE" 2>&1
+sudo apt-get update >> "$LOG_FILE" 2>&1
 
 # Upgrade packages
 echo "Upgrading packages..." >> "$LOG_FILE"
-sudo apt upgrade -y >> "$LOG_FILE" 2>&1
+sudo apt-get upgrade -y >> "$LOG_FILE" 2>&1
 
-echo "Upgrading packages..." >> "$LOG_FILE"
-sudo apt full-upgrade -y >> "$LOG_FILE" 2>&1
+echo "Dist-upgrading packages..." >> "$LOG_FILE"
+sudo apt-get dist-upgrade -y >> "$LOG_FILE" 2>&1
 
 # Remove unnecessary packages
 echo "Removing unused packages..." >> "$LOG_FILE"
-sudo apt autoremove -y >> "$LOG_FILE" 2>&1
+sudo apt-get autoremove -y >> "$LOG_FILE" 2>&1
 
 # Clean package cache
 echo "Cleaning package cache..." >> "$LOG_FILE"
-sudo apt autoclean >> "$LOG_FILE" 2>&1
+sudo apt-get autoclean >> "$LOG_FILE" 2>&1
 
+echo "Cleaning all cache..." >> "$LOG_FILE"
+sudo apt-get clean >> "$LOG_FILE" 2>&1
 
+# Check for reboot
 if [ -f /var/run/reboot-required ]; then
     echo "System Reboot required"
+    echo "System Reboot required" >> "$LOG_FILE"
 else
     echo "No reboot required."
+    echo "No reboot required." >> "$LOG_FILE"
 fi
 
 echo "Maintenance completed successfully: $(date)"
-echo "======================================"
+echo "Maintenance completed successfully: $(date)" >> "$LOG_FILE"
+echo "======================================" >> "$LOG_FILE"
--- Url-Shortner/frontend/script.js
+++ Url-Shortner/frontend/script.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", async function (event) {
 
     try {
         const response = await fetch(
-            "http://127.0.0.1:8000/api/urls",
+            "/api/urls",
             {
                 method: "POST",
 
