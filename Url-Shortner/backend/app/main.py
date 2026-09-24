9db8acf95206e89908f60901fd33a75f96012fce
abdullahhussain706 <abdullah706706@gmail.com>
2026-09-24 15:42:51 +0500

    fix import order

--- Url-Shortner/backend/app/main.py
+++ Url-Shortner/backend/app/main.py
@@ -6,7 +6,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
-
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
